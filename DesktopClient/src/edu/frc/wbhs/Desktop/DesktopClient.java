277b8d308b8658297021de37a9ec2fa49fc6f0a3
Jo <Jo@10.75.8.134>
2014-01-16 16:53:11 -0500

    stuff

--- DesktopClient/src/edu/frc/wbhs/Desktop/DesktopClient.java
+++ DesktopClient/src/edu/frc/wbhs/Desktop/DesktopClient.java
@@ -5,16 +5,45 @@
  */
 
 package edu.frc.wbhs.Desktop;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 /**
  *
  * @author Josh
  */
 public class DesktopClient {
+	
+	NetworkTable Input;
+	NetworkTable Output;
+	
+	double CurrentVelocity,CurrentAngle,TargetAngle,TargetDistance,Mode;
+	
 	public static void main(String[] args)
 	{
 		
 	}
+	
+	public void Start()
+	{
+		NetworkTable.setClientMode();
+		NetworkTable.setIPAddress("10.47.37.2");
+		Input = NetworkTable.getTable("Input");
+		Output = NetworkTable.getTable("Output");
+	}
+	public void UpdateReadings()
+	{
+		CurrentVelocity = Input.getNumber("Velocity", CurrentVelocity);
+		CurrentAngle = Input.getNumber("Angle", CurrentAngle);
+		Mode = Input.getNumber("Target", 0.0);
+	}
+	public void PushReadings()
+	{
+		Output.putNumber("TargetDistance",TargetDistance);
+		Output.putNumber("TargetAngle", TargetAngle);
+	}
+	
+	
+	
 	
 	
 }
